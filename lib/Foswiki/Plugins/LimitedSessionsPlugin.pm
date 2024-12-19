7f582ce04449e234d20b98b2a7604328bf963f4e
MichaelDaum <daum@michaeldaumconsulting.com>
2024-12-19 12:08:38 +0100

    Item15353: added MANIFEST file to package

--- lib/Foswiki/Plugins/LimitedSessionsPlugin.pm
+++ lib/Foswiki/Plugins/LimitedSessionsPlugin.pm
@@ -1,6 +1,6 @@
 # Plugin for Foswiki - The Free and Open Source Wiki, https://foswiki.org/
 #
-# LimitedSessionsPlugin is Copyright (C) 2019-2020 Michael Daum http://michaeldaumconsulting.com
+# LimitedSessionsPlugin is Copyright (C) 2019-2024 Michael Daum http://michaeldaumconsulting.com
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -21,9 +21,10 @@ use warnings;
 use Foswiki::Func ();
 use CGI::Session ();
 
-our $VERSION = '1.01';
-our $RELEASE = '26 Oct 2020';
+our $VERSION = '1.02';
+our $RELEASE = '%$RELEASE%';
 our $SHORTDESCRIPTION = 'Limit the number of open sessions per users';
+our $LICENSECODE = '%$LICENSECODE%';
 our $NO_PREFS_IN_TOPIC = 1;
 our $core;
 
